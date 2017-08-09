82200c8ade2030472af7726b84733b7566496c12
WECP1999 <30815773+WECP1999@users.noreply.github.com>
2017-08-09 15:01:47 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+//inicializa el socket en el puerto 8080
 var io = require('socket.io').listen(8080).sockets;
-
+//almacena los usuarios qeu han estado
 var users = [];
 
+//cuando detectan una conexión socket
 io.on('connection', function(sck){
+	//cuando detecta datos
 	sck.on('envio', function(data){
+		//datos resividos los reenvia
 		io.emit('recieve', data);
 	})
 })
