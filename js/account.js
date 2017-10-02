32898b089eb28e5fcb8de8053599a8fab69c7368
HersonCastillo <30848872+HersonCastillo@users.noreply.github.com>
2017-10-01 20:37:05 -0600

    Ultimo

--- js/account.js
+++ js/account.js
@@ -1,4 +1,7 @@
 $().ready(function(){
+	var NombreUsuario = "",
+		Efectivo = 0e0,
+		Cuentas = [];
 	$(".frm").on("submit", function(){
 		return false;
 	})
@@ -13,10 +16,21 @@ $().ready(function(){
 		if(r !== "" && r !== false) return true;
 		else return false;
 	}
+	var NumVal = (x) =>{
+		if(!isNaN(x) && x.length >= 1) return true;
+		else return false;
+	}
 	var Oc = () => {
 		$("#ventanaregistro").css({visibility: "visible"});
 		$(".container").css({visibility: "hidden"});
 		$("#bgventana").css({visibility: "hidden"});
+	}
+	var Mo = () => {
+		$(".contenido").css({visibility: "hidden"});
+		$(".container").css({visibility: "hidden"});
+		$("#ventanaregistro").css({display: "none"});
+		$(".conta").css({visibility: "visible"});
+		$(".cabe").css({display: "block"});
 	}
 	$("#boton").on("click", function(){
 
@@ -31,10 +45,6 @@ $().ready(function(){
 		$fNacimiento = $("#userdate").val();
 		$Respuesta = $("#userpreg").val();
 
-		$NombreUsuario = "";
-		$Efectivo = 0e0;
-		$Cuentas = [];
-
 		if(	Vald($Nombre) &&
 			Vald($Apellido) &&
 			Vald($Direccion) &&
@@ -42,8 +52,27 @@ $().ready(function(){
 			Vald($Email) && 
 			Vald($Respuesta)
 		){
+			//Validar celular, dui, nit, fnacimiento
 			Oc();
 		}else alert("Datos ingresados, no están completados o vacíos.");
+
+	})
+	$("#ing").on("click", function(){
+		$Username = $("#username").val();
+		$Efectivo = $("#efectivo").val();
+
+		$BancoDefecto = $("#Banco").val();
+		$CuentaDefecto = $("#Cuenta").val();
+		$SaldoDefecto = $("#sald").val();
+
+		if(	Vald($Username) &&
+			NumVal($Efectivo) &&
+			Vald($BancoDefecto) &&
+			NumVal($CuentaDefecto) &&
+			NumVal($SaldoDefecto)
+		){
+			
+		}else alert('Hay datos que rellenar o algún dato no es válido.');
 
 	})
 })
--- js/modal.js
+++ js/modal.js
@@ -13,11 +13,3 @@ $("#agre").on("click", function(){
 	$("#mcuenta").html($("#mcuenta").html() + "<input type='text' name='Banco"+a+"' placeholder='Banco'> <input type='text' name='Cuenta"+a+"' placeholder='# de Cuenta'> <input type='text' name='sald"+a+"' placeholder='Saldo Actual'><br>");
 	d.getElementById('ventanaregistro').style.height = String(i+"px");
 });
-$("#ing").on("click", function(){
-	$(".contenido").css({visibility: "hidden"});
-	$(".container").css({visibility: "hidden"});
-	$("#ventanaregistro").css({display: "none"});
-	$(".conta").css({visibility: "visible"});
-	$(".cabe").css({display: "block"});
-	return false;
-})
