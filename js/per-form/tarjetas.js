9ca4751f0b9bea9558e6db730ec5105f411aca9f
HersonCastillo <30848872+HersonCastillo@users.noreply.github.com>
2017-10-24 16:49:24 -0600

    :100:

--- js/per-form/tarjetas.js
+++ js/per-form/tarjetas.js
@@ -2,9 +2,8 @@ var info = MyInfo();
 
 var tarjeta = info.Tarjetas;
 
-if(tarjeta.length >= 1){
-
-}else $("#cue").html("No posees tarjetas");
+if(tarjeta.length >= 1) for(var i = 0; i<=(tarjeta.length - 1); i++) $("#cue").append("<li>Tarjeta #" + parseInt(i+1) +" <b>" + tarjeta[i].Banco +"</b> <i>" + tarjeta[i].Tarjeta +"</i> <b>-$</b>" + tarjeta[i].Saldo +"</li>");
+else $("#cue").html("No posees tarjetas");
 
 $("#mete").on('click', function(){
 	$Banco = $("#Banco").val();
@@ -26,7 +25,16 @@ $("#mete").on('click', function(){
 			Porcentaje: $PorcentajeInteres,
 			FechaPagoMensual: $PagoPorMes
 		};
-
+		update = JSON.parse(DB.users);
+		id = DB.getItem('log-in');
+		for(var i = 0; i <= (update.length - 1); i++){
+			if(update[i].NombreUsuario == id){
+				update[i].Tarjetas.push($New);
+				DB.users = JSON.stringify(update);
+				break;
+			}
+		}
 		alert('Tarjeta agregada exitosamente');
+		location.reload();
 	}else alert('Algún campo posee un valor no válido');
 })
