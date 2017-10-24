080e35fb69b197dbb66234ebabb18f7d4ccff6d0
HersonCastillo <30848872+HersonCastillo@users.noreply.github.com>
2017-10-24 15:56:53 -0600

    :+1:

--- js/per-form/cuentas.js
+++ js/per-form/cuentas.js
@@ -7,9 +7,7 @@ var count = $Cuentas.length / 3;
 for(var i = 0; i <= (count - 1); i++) $("#cue").append("Cuenta #" + parseInt(i + 1) + " <b>" + $Cuentas[i * 3] + " " + $Cuentas[ (i * 3) + 1 ] + "  $" + $Cuentas[(i * 3) + 2] + "</b><br>")
 
 $("#ingre").on('click', function(){
-	$BancoAux = [];
-	$CuentaAux = [];
-	$SaldoAux = [];
+	$Unico = [];
 
 	$Count = $("#contador").val();
 
@@ -18,10 +16,11 @@ $("#ingre").on('click', function(){
 		$CA = $("#Cuenta" + j).val();
 		$SA = $("#sald" + j).val();
 
-		if($BA != "" && parseFloat($BA) != 0) $BancoAux.push($BA);
-		if($CA != "" && parseFloat($CA) != 0) $CuentaAux.push($CA);
-		if($SA != "" && parseFloat($SA) != 0) $SaldoAux.push($SA);
+		if($BA != "" && parseFloat($BA) != 0) $Unico.push($BA);
+		if($CA != "" && parseFloat($CA) != 0) $Unico.push($CA);
+		if($SA != "" && parseFloat($SA) != 0) $Unico.push($SA);
 	}
 
-	console.log($BancoAux)
+	console.log($Unico)
+
 })
